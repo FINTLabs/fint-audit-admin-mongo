a9c810deeb93cc3ab992a87af262664c3010add7
Frode Sjovatsen <frode@sjovatsen.net>
2016-11-15 15:47:06 +0100

    added crossorigin to rest controller

--- src/main/java/no/fint/audit/plugin/mongo/admin/controller/AdminController.java
+++ src/main/java/no/fint/audit/plugin/mongo/admin/controller/AdminController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Slf4j
+@CrossOrigin("*")
 @RequestMapping(value = "/audit/events", produces = MediaType.APPLICATION_JSON_VALUE)
 @RestController
 public class AdminController {
