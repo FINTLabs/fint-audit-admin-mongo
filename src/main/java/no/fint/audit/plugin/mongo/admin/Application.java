c00faefb8d2bf5df5916018841f17f43af88a125
Frode Sjovatsen <frode@sjovatsen.net>
2016-12-05 08:43:51 +0100

    added HAL HATEOS support

--- src/main/java/no/fint/audit/plugin/mongo/admin/Application.java
+++ src/main/java/no/fint/audit/plugin/mongo/admin/Application.java
@@ -1,12 +1,14 @@
 package no.fint.audit.plugin.mongo.admin;
 
+import no.rogfk.hateoas.extension.annotations.EnableHalHypermediaSupport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableHalHypermediaSupport
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- src/main/java/no/fint/audit/plugin/mongo/admin/controller/AdminController.java
+++ src/main/java/no/fint/audit/plugin/mongo/admin/controller/AdminController.java
@@ -4,9 +4,12 @@ package no.fint.audit.plugin.mongo.admin.controller;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.audit.plugin.mongo.admin.model.MongoAuditEvent;
 import no.fint.audit.plugin.mongo.admin.repository.AuditEventMongoRepository;
+import no.rogfk.hateoas.extension.HalPagedResources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import no.rogfk.hateoas.extension.annotations.HalResource;
+
 
 import java.util.List;
 
@@ -19,35 +22,45 @@ public class AdminController {
     @Autowired
     AuditEventMongoRepository auditEventMongoRepository;
 
+    @HalResource(pageSize = 100)
     @RequestMapping(method = RequestMethod.GET)
-    public List<MongoAuditEvent> getAllAuditEvents() {
-        return auditEventMongoRepository.getAllAuditEvents();
+    public HalPagedResources<MongoAuditEvent> getAllAuditEvents(@RequestParam(required = false) Integer page) {
+        return new HalPagedResources<>(auditEventMongoRepository.getAllAuditEvents(), page);
     }
 
 
+    @HalResource(pageSize = 100)
     @RequestMapping(value = "/source/{source}", method = RequestMethod.GET)
-    public List<MongoAuditEvent> getAllSourceAuditEvents(@PathVariable String source) {
-        return auditEventMongoRepository.getAllAuditEventsBySource(source);
+    public HalPagedResources<MongoAuditEvent> getAllSourceAuditEvents(@PathVariable String source, @RequestParam(required = false) Integer page) {
+        return new HalPagedResources<>(auditEventMongoRepository.getAllAuditEventsBySource(source), page);
     }
 
+    @HalResource(pageSize = 100)
     @RequestMapping(value = "/corrid/{corrId}", method = RequestMethod.GET)
-    public List<MongoAuditEvent> getAllAuditEventsByCorrId(@PathVariable String corrId) {
-        return auditEventMongoRepository.getAllAuditEventsByCorrId(corrId);
+    public HalPagedResources<MongoAuditEvent> getAllAuditEventsByCorrId(@PathVariable String corrId, @RequestParam(required = false) Integer page) {
+        return new HalPagedResources<>(auditEventMongoRepository.getAllAuditEventsByCorrId(corrId), page);
     }
 
+    @HalResource(pageSize = 100)
     @RequestMapping(value = "org", method = RequestMethod.GET)
-    public List<MongoAuditEvent> getOrgAuditEvents(@RequestHeader("x-org-id") String orgId) {
-        return auditEventMongoRepository.getOrgAuditEvents(orgId);
+    public HalPagedResources<MongoAuditEvent> getOrgAuditEvents(@RequestHeader("x-org-id") String orgId, @RequestParam(required = false) Integer page) {
+        return new HalPagedResources<>(auditEventMongoRepository.getOrgAuditEvents(orgId), page);
     }
 
+    @HalResource(pageSize = 100)
     @RequestMapping(value = "org/source/{source}", method = RequestMethod.GET)
-    public List<MongoAuditEvent> getOrgAuditEventsBySource(@RequestHeader("x-org-id") String orgId, @PathVariable String source) {
-        return auditEventMongoRepository.getOrgAuditEventsBySource(orgId, source);
+    public HalPagedResources<MongoAuditEvent> getOrgAuditEventsBySource(@RequestHeader("x-org-id") String orgId,
+                                                           @PathVariable String source,
+                                                           @RequestParam(required = false) Integer page) {
+        return new HalPagedResources<>(auditEventMongoRepository.getOrgAuditEventsBySource(orgId, source), page);
     }
 
+    @HalResource(pageSize = 100)
     @RequestMapping(value = "org/corrid/{corrId}", method = RequestMethod.GET)
-    public List<MongoAuditEvent> getOrgAuditEventsByCorrId(@RequestHeader("x-org-id") String orgId, @PathVariable String corrId) {
-        return auditEventMongoRepository.getOrgAuditEventsByCorrId(orgId, corrId);
+    public HalPagedResources<MongoAuditEvent> getOrgAuditEventsByCorrId(@RequestHeader("x-org-id") String orgId,
+                                                           @PathVariable String corrId,
+                                                           @RequestParam(required = false) Integer page) {
+        return new HalPagedResources<>(auditEventMongoRepository.getOrgAuditEventsByCorrId(orgId, corrId), page);
     }
 
 
